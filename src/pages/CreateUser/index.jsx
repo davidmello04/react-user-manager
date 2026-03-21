f84e3769de3b9608d8915f674217d2a45163b0ed
David Mello <davidbezerra10@hotmail.com>
2026-03-21 15:47:31 -0300

    feat: implement user creation form and routing

--- src/pages/CreateUser/index.jsx
+++ src/pages/CreateUser/index.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
+
 function CreateUser() {
-    return <h1>Criar Usuário</h1>;
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //Impede o comportamento padrão do formulário de recarregar a página
+
+        const newUser = {
+            name,
+            email
+        };
+
+        console.log(newUser);
+    };
+
+    return (
+        <div>
+            <h1>Create User</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="Name"
+                    value={name}  
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <input
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateUser;
